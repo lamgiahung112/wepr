939a574c5adc5fff25b5b1fd66411a4209d6b5a1
bananaback <58079308+bananaback@users.noreply.github.com>
2023-12-09 02:10:07 +0700

    create folder for static resources

--- src/main/java/hcmute/wepr/ielts_app/IeltsAppApplication.java
+++ src/main/java/hcmute/wepr/ielts_app/IeltsAppApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import hcmute.wepr.ielts_app.Models.enums.Role;
 import hcmute.wepr.ielts_app.Services.Interfaces.UserServiceInterface;
 
 @SpringBootApplication
@@ -17,6 +18,7 @@ public class IeltsAppApplication {
 	@Bean
 	CommandLineRunner runner(UserServiceInterface userService) {
 		return args -> {
+			userService.createUser("tin", "tin123", Role.ROLE_TEACHER, "votrongtin882003@gmail.com", 0);
 		};
 	}
 }
