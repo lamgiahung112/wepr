062c67bba216ee8dd6eeede7034781c003d8443f
Lam Gia Hung <lamgiahung112@gmail.com>
2023-12-13 04:25:28 +0700

    Update RatingService.java

--- src/main/java/hcmute/wepr/ielts_app/Services/RatingService.java
+++ src/main/java/hcmute/wepr/ielts_app/Services/RatingService.java
@@ -15,6 +15,9 @@ public class RatingService implements RatingServiceInterface {
     @Autowired
     private RatingRepositoryInterface ratingRepository;
 
+	@Autowired
+	private CourseRepositoryInterface courseRepository;
+
     @Override
     public List<Rating> getAllRatings() {
         return ratingRepository.findAll();
@@ -22,8 +25,22 @@ public class RatingService implements RatingServiceInterface {
 
 	@Override
 	public void rateCourse(RateCourseRequest request) {
-		// TODO Auto-generated method stub
-		
+		Rating rating = Rating.builder()
+				.ratingId(new RatingId().setCourseId(request.getCourseId()).setUserId(request.getUserId()))
+				.rating(request.getRating()).comment(request.getComment())
+				.build();
+		ratingRepository.save(rating);
+		AtomicInteger totalRatingPoint = new AtomicInteger(0);
+		AtomicInteger totalRatingCount = new AtomicInteger(0);
+		ratingRepository.findByRatingIdCourseId(request.getCourseId())
+			.stream()
+			.forEach(r -> {
+				totalRatingCount.getAndIncrement();
+				totalRatingPoint.getAndAdd(r.getRating());
+			});
+		Course course = courseRepository.findById(request.getCourseId()).orElse(null);
+		course.setRating(totalRatingPoint.get()/totalRatingCount.get());
+		courseRepository.save(course);
 	}
 
     // Các phương thức khác nếu cần (lấy đánh giá theo ID, cập nhật đánh giá, xóa đánh giá, ...)
