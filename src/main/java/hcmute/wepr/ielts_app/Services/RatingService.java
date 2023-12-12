9bf7a401e7da6d02ad354fb5e837a81c6e3e01ec
Hung <Lam>
2023-12-13 04:29:32 +0700

    import

--- src/main/java/hcmute/wepr/ielts_app/Services/RatingService.java
+++ src/main/java/hcmute/wepr/ielts_app/Services/RatingService.java
@@ -1,13 +1,17 @@
 package hcmute.wepr.ielts_app.Services;
 
+import hcmute.wepr.ielts_app.Models.Course;
 import hcmute.wepr.ielts_app.Models.Rating;
+import hcmute.wepr.ielts_app.Models.RatingId;
 import hcmute.wepr.ielts_app.Services.Interfaces.RatingServiceInterface;
 import hcmute.wepr.ielts_app.Utilities.Requests.RateCourseRequest;
+import hcmute.wepr.ielts_app.repositories.CourseRepositoryInterface;
 import hcmute.wepr.ielts_app.repositories.RatingRepositoryInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class RatingService implements RatingServiceInterface {
