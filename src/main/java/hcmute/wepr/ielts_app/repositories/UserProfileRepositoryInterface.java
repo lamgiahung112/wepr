0f3d00d011854621442cdea546fab2d2f05d051a
hidesusanto <vosonhung03@gmail.com>
2023-12-13 05:27:33 +0700

    fix UI and add studentcontroller

--- src/main/java/hcmute/wepr/ielts_app/repositories/UserProfileRepositoryInterface.java
+++ src/main/java/hcmute/wepr/ielts_app/repositories/UserProfileRepositoryInterface.java
@@ -1,6 +1,7 @@
 package hcmute.wepr.ielts_app.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import hcmute.wepr.ielts_app.Models.UserProfile;
 
