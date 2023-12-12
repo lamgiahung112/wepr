8863c9ff35cd6a5edd96f2bdba65af54bb307ae1
bananaback <58079308+bananaback@users.noreply.github.com>
2023-12-13 04:06:52 +0700

    fix answer attemp bug

---
+++ src/main/java/hcmute/wepr/ielts_app/Utilities/responses/AnswerAttemptResponse.java
@@ -0,0 +1,23 @@
+package hcmute.wepr.ielts_app.Utilities.responses;
+
+import java.time.LocalDateTime;
+
+import hcmute.wepr.ielts_app.Models.enums.StudentAnswerStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class AnswerAttemptResponse {
+	private int exerciseId;
+	private int userId;
+	private int answerId;
+	private String answer;
+	private LocalDateTime createAt;
+	private StudentAnswerStatus studentAnswerStatus;
+	private WritingCorrectionResponse writingCorrectionResponse;
+}
