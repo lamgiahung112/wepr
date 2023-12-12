bfa5ed087f1be3275a934c6ffe6fee59ab4d3ac3
Hung <Lam>
2023-12-13 05:33:31 +0700

    update profile teacher

--- src/main/java/hcmute/wepr/ielts_app/Services/Interfaces/TeacherServiceInterface.java
+++ src/main/java/hcmute/wepr/ielts_app/Services/Interfaces/TeacherServiceInterface.java
@@ -3,9 +3,10 @@ package hcmute.wepr.ielts_app.Services.Interfaces;
 import hcmute.wepr.ielts_app.Models.ApplicationUser;
 import hcmute.wepr.ielts_app.Models.UserProfile;
 import hcmute.wepr.ielts_app.Utilities.Requests.SignUpTeacherRequest;
+import hcmute.wepr.ielts_app.Utilities.Requests.UpdateTeacherRequest;
 
 public interface TeacherServiceInterface {
 	UserProfile createTeacherProfile(ApplicationUser user, SignUpTeacherRequest request);
 	UserProfile getTeacherByID (Integer id);
-	UserProfile updateTeacherProfile (Integer id, UserProfile updateTeacher);
+	void updateTeacherProfile(int userId, UpdateTeacherRequest request);
 }
